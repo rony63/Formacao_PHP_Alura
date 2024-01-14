1628f37c594718ce4c8a50cc0370a5469a0f65f9
rony63 <ronymcall@gmail.com>
2024-01-13 22:49:25 -0300

    Aprendendo sobre tipos primitivos

---
+++ tipos.php
@@ -0,0 +1,21 @@
+<?php 
+  //Variaveis
+  $idade = 23;
+  $salario = 1001.35;
+  $texto = "Ola!";
+  $divisao = 10 / 3;
+  $verdadeiro = true;
+
+  //Mostrando os tipos da variaveis criadas acima
+  echo gettype($texto);
+  echo "\n";
+  echo gettype($salario);
+  echo "\n";
+  echo gettype($texto);
+  echo "\n";
+  echo gettype($divisao);
+  echo "\n";
+  echo gettype($verdadeiro);
+  echo "\n";
+   
+?>
