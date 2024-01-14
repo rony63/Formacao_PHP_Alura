292fb7d7fb11be8615d755cc0f48e910306404d1
rony63 <ronymcall@gmail.com>
2024-01-13 22:36:41 -0300

    Aprendendo sobre operacoes

---
+++ operacoes.php
@@ -0,0 +1,16 @@
+<?php 
+  $soma = 2 + 3;
+  $subtracao = 2 - 2;
+  $multiplicacao = 2 * 3;
+  $divisao = 10 / 2;
+
+  echo $soma;
+  echo "\n";
+  echo $subtracao;
+  echo "\n";
+  echo $multiplicacao;
+  echo "\n";
+  echo $divisao;
+  echo "\n";
+
+?>
